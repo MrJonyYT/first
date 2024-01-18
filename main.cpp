8a7ef5f6fc3611b96789770b5077b82141f0bde8
moonangel <ppetrovich349@gmail.com>
2024-01-18 16:15:31 +0300

    meowanime

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    char c = s[0];
+    if(c>=97 && c<=122)
+    {
+        c=c-32;
+        cout << c;
+    }else if(c>=65 && c<=90)
+    {
+        c=c+32;
+        cout << c;
+    }else
+    {
+        cout<<"nizya takoe";
+    }
 }
